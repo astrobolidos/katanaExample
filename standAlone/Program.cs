656250fb07e1dae9dab8500946d610e9e8950983
astrobolidos <lisandroalavez@gmail.com>
2014-01-19 20:09:42 +0000

    initial commit

---
+++ standAlone/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace standAlone
+{
+    using System.IO;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:1978";
+
+            using (WebApp.Start(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping...");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHelloWord();
+            //app.Use<HelloWorldComponent>();
+
+            //app.UseWelcomePage();
+
+            //app.Run(ctx => {
+            //    return ctx.Response.WriteAsync("hello world");
+            //});
+        }
+    }
+
+    public static class AppBuilderExtensions
+    {
+        public static void UseHelloWord(this IAppBuilder app)
+        {
+            app.Use<HelloWorldComponent>();
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        public AppFunc _next { get; set; }
+        public HelloWorldComponent(AppFunc next)
+        {
+            this._next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!! using component"); 
+            }
+        }
+    }
+}
