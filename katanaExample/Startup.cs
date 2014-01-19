fa1787a14eb4e43900791ff251e74550e6287ba2
astrobolidos <lisandroalavez@gmail.com>
2014-01-19 23:19:01 +0000

    added missing startup file

---
+++ katanaExample/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(katanaExample.Startup))]
+namespace katanaExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //path logging middleware
+            app.Use(async (env, next) =>
+            {
+                Console.WriteLine("Requesting " + env.Request.Path);
+                await env.Response.WriteAsync("hello world");
+            });
+        }
+    }
+}
