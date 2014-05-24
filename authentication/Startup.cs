2f9a4b5deff50070191dc856ab8305248307289d
astrobolidos <lisandroalavez@gmail.com>
2014-05-24 16:54:39 +0100

    update to use webapi

--- authentication/Startup.cs
+++ authentication/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(authentication.Startup))]
 namespace authentication
@@ -11,7 +12,20 @@ namespace authentication
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuthentication(app);
+
+            ConfigureWebApi(app);
         }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            app.UseWebApi(config);
+        }
+
 
         public void ConfigureAuthentication(IAppBuilder app)
         {
