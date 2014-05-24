dde33ae19266a0da9d2a201825109f550bcda92f
astrobolidos <lisandroalavez@gmail.com>
2014-05-24 16:39:16 +0100

    added files for webapi authentication

---
+++ authentication/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace authentication.Controllers
+{
+    public class InfoController : ApiController
+    {
+        [Authorize]
+        public HttpResponseMessage Get()
+        {
+            var responseContent = new { Id = 1234, Value = "this is a value", Date = DateTime.Now };
+            var response = Request.CreateResponse(System.Net.HttpStatusCode.OK, responseContent);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            return response;
+        }
+    }
+}
