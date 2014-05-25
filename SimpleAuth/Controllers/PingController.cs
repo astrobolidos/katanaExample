aed5735ff0219ff78072f5cadf1f052436d604f3
astrobolidos <lisandroalavez@gmail.com>
2014-05-25 15:34:18 +0100

    adding webapi configuration and account controller

---
+++ SimpleAuth/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace SimpleAuth.Controllers
+{
+    public class PingController : ApiController
+    {
+        //[Authorize]
+        public HttpResponseMessage Get()
+        {
+            var status = HttpStatusCode.OK;
+
+            return Request.CreateResponse(status, DateTime.Now);
+        }
+    }
+}
--- SimpleAuth/Startup.cs
+++ SimpleAuth/Startup.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using System;
 
 [assembly: OwinStartup(typeof(SimpleAuth.Startup))]
 namespace SimpleAuth
 {
-    public class Startup
+    public partial class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            ConfigureWebApi(app);
+        }
     }
 }
